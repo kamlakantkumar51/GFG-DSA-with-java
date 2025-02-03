a81519ba943073cb716be99adeb02b2a7b757d46
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-02-03 14:34:40 +0530

    Create insertionsort.java

---
+++ insertionsort.java
@@ -0,0 +1,17 @@
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        // code here
+        for(int i=1;i<arr.length;i++){
+            int curr = arr[i];
+            int prev = i-1;
+            //to finding correct position
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            //inserting
+            arr[prev+1]=curr;
+        }
+    }
+}
