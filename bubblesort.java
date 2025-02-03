14bb75f8ea44dbe762c7b6db9bfecf04f20dd58f
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-02-03 12:18:51 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,16 @@
+class Solution {
+    // Function to sort the array using bubble sort algorithm.
+    public static void bubbleSort(int arr[]) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                //swapping...
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
